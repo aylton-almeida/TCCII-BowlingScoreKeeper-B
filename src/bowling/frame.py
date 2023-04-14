6d1c20c728cd276d1f80dbcdfd4dd9f587caaef9
Aylton Almeida <almeida@aylton.dev>
2023-04-14 09:08:34 -0300

    ✨ Implement init, score, is_strike and is_spare

--- src/bowling/frame.py
+++ src/bowling/frame.py
@@ -1,29 +1,38 @@
+MAX_SCORE = 10
+
+
 class Frame:
+    first_throw: int
+    second_throw: int
+
     def __init__(self, first_throw: int, second_throw: int) -> None:
+        if first_throw + second_throw > MAX_SCORE:
+            raise Exception("First + Second throws should not equal to more than ten")
+
         self.first_throw = first_throw
         self.second_throw = second_throw
 
     def score(self) -> int:
-        """ The score of a single frame """
-        # To be implemented
-        pass
+        """The score of a single frame"""
+
+        return self.first_throw + self.second_throw
 
     def is_strike(self) -> bool:
-        """ Return whether the frame is a strike or not """
-        # To be implemented
-        pass
+        """Return whether the frame is a strike or not"""
+
+        return self.first_throw == MAX_SCORE
 
     def is_spare(self) -> bool:
-        """ Return whether the frame is a spare or not """
-        # To be implemented
-        pass
+        """Return whether the frame is a spare or not"""
+
+        return self.score() == 10 and self.first_throw < MAX_SCORE
 
     def is_last_frame(self) -> bool:
-        """ Return whether the frame is a last frame of the game """
+        """Return whether the frame is a last frame of the game"""
         # To be implemented
         pass
 
     def bonus(self) -> int:
-        """ Bonus throw """
+        """Bonus throw"""
         # To be implemented
         pass
--- src/bowling/test_frame.py
+++ src/bowling/test_frame.py
@@ -1,9 +1,58 @@
 import unittest
 
+from bowling.frame import Frame
+
 
 class TestFrames(unittest.TestCase):
-    pass
+    def test_Init_when_ScoreSumsToTen(self):
+        frame = Frame(5, 5)
+        self.assertEqual(frame.first_throw, 5)
+        self.assertEqual(frame.second_throw, 5)
+
+    def test_Init_when_ScoreSumsToLessThanTen(self):
+        frame = Frame(3, 4)
+        self.assertEqual(frame.first_throw, 3)
+        self.assertEqual(frame.second_throw, 4)
+
+    def test_Init_when_ScoreSumsToMoreThanTen(self):
+        with self.assertRaises(Exception) as err:
+            Frame(6, 5)
+
+        self.assertEqual(
+            str(err.exception),
+            "First + Second throws should not equal to more than ten",
+        )
+
+    def test_Score(self):
+        self.assertEqual(Frame(3, 6).score(), 9)
+        self.assertEqual(Frame(3, 4).score(), 7)
+        self.assertEqual(Frame(2, 8).score(), 10)
+        self.assertEqual(Frame(10, 0).score(), 10)
+
+    def test_IsStrike_when_FirstThrowIs10(self):
+        frame = Frame(first_throw=10, second_throw=0)
+        self.assertTrue(frame.is_strike())
+
+    def test_IsStrike_when_SecondThrowIs10(self):
+        frame = Frame(first_throw=0, second_throw=10)
+        self.assertFalse(frame.is_strike())
+
+    def test_IsStrike_when_ScoreIsLessThan10(self):
+        frame = Frame(first_throw=2, second_throw=3)
+        self.assertFalse(frame.is_strike())
+
+    def test_IsSpare_when_ScoreIs10(self):
+        frame = Frame(first_throw=2, second_throw=8)
+        self.assertTrue(frame.is_spare())
+
+    def test_IsSpare_when_ItIsStrike(self):
+        frame = Frame(first_throw=10, second_throw=0)
+        self.assertFalse(frame.is_spare())
+
+    def test_IsSpare_when_ScoreIsLessThan10(self):
+        frame = Frame(first_throw=6, second_throw=1)
+        self.assertFalse(frame.is_spare())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
